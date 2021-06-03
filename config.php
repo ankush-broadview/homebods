c3d411c8c9b200ac418da6113fcfe2a7308c33f7
bikalpit <60959253+ankush-broadview@users.noreply.github.com>
2021-06-03 19:14:07 +0530

    Ankush
    Change Database Configuration

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 	class cleanto_myvariable{
 	public $hostnames = "localhost";
-	public $username = "root";
-	public $passwords = "";
+	public $username = "homebods_cleanto";
+	public $passwords = "Ucanwin007))&";
 	public $database = "homebods_cleanto";
 	public $epcode = "VinodAhuja";
 } ?>
