9a2f5882cdb7bbe7c7d6da6e8fc4fc5fd746afc3
ankush-broadview <60959253+ankush-broadview@users.noreply.github.com>
2021-11-11 16:12:01 +0530

    Stock photo is still  not replaced by the logo - Done

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
 <?php
 
-require_once('env.php');	
+//require_once('env.php');	
 	class cleanto_myvariable {
-		public $hostnames = DB_HOST;
-		public $username = DB_USERNAME;
-		public $passwords = DB_PASSWORD;
-		public $database =DATABASE;
-		public $epcode = EPCODE;
+		public $hostnames = 'localhost';
+		public $username = 'root';
+		public $passwords = '';
+		public $database = 'cleanto_homebods';
+		public $epcode = 'VinodAhuja';
 	}
 
 ?>
